69f5974326e70caa8e823d402c281eb3e9c0e1a1
glunaJaime <43741944+glunaJaime@users.noreply.github.com>
2018-12-26 13:14:45 +0100

    Update uaserver.py
    pasado pep8 a server

--- uaserver.py
+++ uaserver.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import os,sys,socketserver
-
-from proxy_registrar import Log_Writer,read_config_file,dtd_ua
+import os
+import sys
+import socketserver
+from proxy_registrar import Log_Writer, read_config_file, dtd_ua
 
 usage_error = 'usage error: python3 uaserver.py config'
 
@@ -16,7 +17,7 @@ class ServerHandler(socketserver.DatagramRequestHandler):
         line = ''
         ip = self.client_address[0]
         port = self.client_address[1]
-        log.received_from(ip,str(port),data.replace('\r\n',' '))
+        log.received_from(ip, str(port), data.replace('\r\n', ' '))
         print(data)
         if 'invite' in data.lower():
             self.get_mp32rtp(data)
@@ -24,14 +25,15 @@ class ServerHandler(socketserver.DatagramRequestHandler):
             line += 'SIP/2.0 180 Ringing\r\n\r\n'
             line += 'SIP/2.0 200 OK\r\n'
             line += 'Content-Type: application/sdp\r\n\r\n'
-            line += 'v=0\r\no=' + config['account_username'] + ' ' + config['uaserver_ip']
-            line += '\r\ns=sesionextraordinaria\r\nt=0\r\n'
-            line += 'm=audio ' + config['rtpaudio_puerto'] + ' RTP\r\n'
+            line += 'v=0\r\no=' + config['account_username'] + ' '
+            line += config['uaserver_ip'] + '\r\ns=sesionextraordinaria\r\n'
+            line += 't=0\r\nm=audio ' + config['rtpaudio_puerto'] + ' RTP\r\n'
         elif 'ack' in data.lower():
             if len(self.sesion_data) != 0:
-                mp32rtp = './mp32rtp -i ' + self.sesion_data[0] + ' -p ' 
+                mp32rtp = './mp32rtp -i ' + self.sesion_data[0] + ' -p '
                 mp32rtp += self.sesion_data[1] + ' < ' + config['audio_path']
-                cvlc = 'cvlc rtp://@ ' + self.sesion_data[0] + ':' + self.sesion_data[1]
+                address = self.sesion_data[0] + ':' + self.sesion_data[1]
+                cvlc = 'cvlc rtp://@ ' + address
                 os.system(mp32rtp + ' & ' + cvlc)
                 self.sesion_data = []
                 line = ''
@@ -41,10 +43,10 @@ class ServerHandler(socketserver.DatagramRequestHandler):
             line = 'SIP/2.0 405 Method not Allowed\r\n'
 
         if line:
-            log.sent_to(ip,str(port),line.replace('\r\n',' '))
-            self.wfile.write(bytes(line,'utf-8') + b'\r\n')
+            log.sent_to(ip, str(port), line.replace('\r\n', ' '))
+            self.wfile.write(bytes(line, 'utf-8') + b'\r\n')
 
-    def get_mp32rtp(self,data):
+    def get_mp32rtp(self, data):
         ip = data.split('\r\n')[4].split()[-1]
         port = data.split('\r\n')[7].split()[1]
         self.sesion_data.append(ip)
@@ -56,12 +58,12 @@ if __name__ == "__main__":
         sys.exit(usage_error)
     else:
         xml_file = sys.argv[1]
-        
-    config = read_config_file(dtd_ua,xml_file)
-    log = Log_Writer(config['log_path'],'%Y%m%d%H%M%S')
-    address = (config['uaserver_ip'],int(config['uaserver_puerto']))
-    
-    uaserver = socketserver.UDPServer(address,ServerHandler)
+
+    config = read_config_file(dtd_ua, xml_file)
+    log = Log_Writer(config['log_path'], '%Y%m%d%H%M%S')
+    address = (config['uaserver_ip'], int(config['uaserver_puerto']))
+
+    uaserver = socketserver.UDPServer(address, ServerHandler)
     log.starting()
     print('Server listening at ' + address[0] + ':' + str(address[1]))
     try:
