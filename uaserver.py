a410cb5c9f2b264e5ce0f509f398a7b1c8e275a0
glunaJaime <43741944+glunaJaime@users.noreply.github.com>
2018-12-26 11:33:24 +0100

    Update uaserver.py
    implementada funcionalidad en el servidor

--- uaserver.py
+++ uaserver.py
@@ -1,2 +1,72 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+
+import os,sys,socketserver
+
+from proxy_registrar import Log_Writer,read_config_file,dtd_ua
+
+usage_error = 'usage error: python3 uaserver.py config'
+
+
+class ServerHandler(socketserver.DatagramRequestHandler):
+    sesion_data = []
+
+    def handle(self):
+        data = self.rfile.read().decode('utf-8')
+        line = ''
+        ip = self.client_address[0]
+        port = self.client_address[1]
+        log.received_from(ip,str(port),data.replace('\r\n',' '))
+        print(data)
+        if 'invite' in data.lower():
+            self.get_mp32rtp(data)
+            line = 'SIP/2.0 100 Trying\r\n\r\n'
+            line += 'SIP/2.0 180 Ringing\r\n\r\n'
+            line += 'SIP/2.0 200 OK\r\n'
+            line += 'Content-Type: application/sdp\r\n\r\n'
+            line += 'v=0\r\no=' + config['account_username'] + ' ' + config['uaserver_ip']
+            line += '\r\ns=sesionextraordinaria\r\nt=0\r\n'
+            line += 'm=audio ' + config['rtpaudio_puerto'] + ' RTP\r\n'
+        elif 'ack' in data.lower():
+            if len(self.sesion_data) != 0:
+                mp32rtp = './mp32rtp -i ' + self.sesion_data[0] + ' -p ' 
+                mp32rtp += self.sesion_data[1] + ' < ' + config['audio_path']
+                cvlc = 'cvlc rtp://@ ' + self.sesion_data[0] + ':' + self.sesion_data[1]
+                os.system(mp32rtp)
+                os.system(cvlc)
+                self.mp32rtp = []
+                line = ''
+        elif 'bye' in data.lower():
+            line = 'SIP/2.0 200 OK\r\n'
+        else:
+            line = 'SIP/2.0 405 Method not Allowed\r\n'
+
+        if line:
+            log.sent_to(ip,str(port),line.replace('\r\n',' '))
+            self.wfile.write(bytes(line,'utf-8') + b'\r\n')
+
+    def get_mp32rtp(self,data):
+        ip = data.split('\r\n')[4].split()[-1]
+        port = data.split('\r\n')[7].split()[1]
+        self.mp32rtp.append(ip)
+        self.mp32rtp.append(port)
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 2:
+        sys.exit(usage_error)
+    else:
+        xml_file = sys.argv[1]
+        
+    config = read_config_file(dtd_ua,xml_file)
+    log = Log_Writer(config['log_path'],'%Y%m%d%H%M%S')
+    address = (config['uaserver_ip'],int(config['uaserver_puerto']))
+    
+    uaserver = socketserver.UDPServer(address,ServerHandler)
+    log.starting()
+    print('Server listening at ' + address[0] + ':' + str(address[1]))
+    try:
+        uaserver.serve_forever()
+    except KeyboardInterrupt:
+        log.finishing()
+        print("Finalizado servidor")
